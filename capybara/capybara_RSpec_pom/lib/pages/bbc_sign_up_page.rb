06fbc2ecb6a54f33d84ccbb351bfbf9872072642
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-12-19 22:12:19 +0000

    finished sign up error tests

--- capybara/capybara_RSpec_pom/lib/pages/bbc_sign_up_page.rb
+++ capybara/capybara_RSpec_pom/lib/pages/bbc_sign_up_page.rb
@@ -10,6 +10,8 @@ class BbcSignUpPage
  EMAIL_FIELD_ID = 'user-identifier-input'
  PASSWORD_FIELD_ID = 'password-input'
  POSTCODE_FIELD_ID = 'postcode-input'
+ ALREADY_USED_EMAIL_ERROR_ID = 'form-message-email'
+ INCORRECT_PASSWORD_ERROR_ID = 'form-message-password'
 
   def day_field
     find_field(DAY_FIELD_ID)
@@ -69,6 +71,26 @@ class BbcSignUpPage
 
   def select_gender
     select('Female', from: 'gender-input')
+  end
+
+  def select_opt(opt)
+    find(:xpath, ".//label[@for='#{opt}']/div/div").click
+  end
+
+  def find_register_button
+    find_button(SUBMIT_BUTTON_ID)
+  end
+
+  def click_register_button
+    find_register_button.click
+  end
+
+  def find_email_error
+    find(:id, ALREADY_USED_EMAIL_ERROR_ID).text
+  end
+
+  def find_password_error
+    find(:id, INCORRECT_PASSWORD_ERROR_ID).text
   end
 
 end
--- capybara/capybara_RSpec_pom/spec/bbc_sign_up_failure_spec.rb
+++ capybara/capybara_RSpec_pom/spec/bbc_sign_up_failure_spec.rb
@@ -21,7 +21,28 @@ describe 'Incorrect sign up details produces valid error' do
       @bbc_site.bbc_sign_up_page.fill_in_password('password123')
       @bbc_site.bbc_sign_up_page.fill_in_postcode('TW10 6TF')
       @bbc_site.bbc_sign_up_page.select_gender
+      @bbc_site.bbc_sign_up_page.select_opt('optOut')
+      @bbc_site.bbc_sign_up_page.click_register_button
+      expect(@bbc_site.bbc_sign_up_page.find_email_error).to eq('Looks like you’ve already registered with this email. Want to sign in?')
+      sleep 4
+    end
 
+    it "should produce an error when the password submmitted breaks the rules" do
+      @bbc_site = BbcSite.new
+      @bbc_site.bbc_homepage.visit_home_page
+      @bbc_site.bbc_homepage.click_sign_in_link
+      @bbc_site.bbc_sign_in_page.click_register_link
+      @bbc_site.bbc_sign_up_page.fill_in_day('26')
+      @bbc_site.bbc_sign_up_page.fill_in_month('04')
+      @bbc_site.bbc_sign_up_page.fill_in_year('1996')
+      @bbc_site.bbc_sign_up_page.click_sign_up_button
+      @bbc_site.bbc_sign_up_page.fill_in_email('dray@email.com')
+      @bbc_site.bbc_sign_up_page.fill_in_password('password')
+      @bbc_site.bbc_sign_up_page.fill_in_postcode('TW10 6TF')
+      @bbc_site.bbc_sign_up_page.select_gender
+      @bbc_site.bbc_sign_up_page.select_opt('optOut')
+      @bbc_site.bbc_sign_up_page.click_register_button
+      expect(@bbc_site.bbc_sign_up_page.find_password_error).to eq('Sorry, that password isn\'t valid. Please include something that isn\'t a letter.')
       sleep 4
     end
 
