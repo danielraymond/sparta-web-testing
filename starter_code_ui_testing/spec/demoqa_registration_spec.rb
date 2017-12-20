3e5598a2f1054cfc36fbf865d5b28242dc717113
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-12-20 10:50:15 +0000

    completed tests for selenium form lab

--- starter_code_ui_testing/spec/demoqa_registration_spec.rb
+++ starter_code_ui_testing/spec/demoqa_registration_spec.rb
@@ -27,12 +27,10 @@ describe 'testing the demoqa registration page' do
 
     it 'should accept a marital status selection of Single, Married, or Divorced' do
       expect(@driver.generate_random_marital_status).to be true
-      sleep 1
     end
 
     it 'should accept a hobby status selection of Dance, Reading, or Cricket' do
       expect(@driver.generate_random_hobby).to be true
-      sleep 1
     end
 
     it 'should have a country default of Afghanistan' do
@@ -40,41 +38,48 @@ describe 'testing the demoqa registration page' do
     end
 
     it 'accept a new DOB' do
-      pending("code")
+      @driver.dob_month_list_select('4')
+      @driver.dob_day_list_select('26')
+      @driver.dob_year_list_select('1996')
     end
 
     it 'should accept a new country value' do
       x = @driver.country_dropdown_list_select('Egypt')
       expect(x).to be_kind_of(String)
-      sleep 1
     end
 
     it 'should accept a valid phone number' do
       @driver.set_phone_number_field(7624163823)
       expect(@driver.get_phone_number_field_value).to be_kind_of(String)
-      sleep 1
     end
 
     it 'should accept a username' do
       @driver.set_user_name_field('draymond')
       expect(@driver.get_user_name_field_value).to be_kind_of(String)
-      sleep 1
     end
 
-    it 'should accept a about yourself text' do
+    it 'should accept an email' do
       @driver.set_email_field('email@email.com')
       expect(@driver.get_email_field_value).to be_kind_of(String)
-      sleep 1
+    end
+
+    it "should accept an about yourself" do
+      @driver.set_about_yourself_field('I am a person')
+      expect(@driver.get_about_yourself_value).to eq('I am a person')
     end
 
     it 'should accept a password' do
       @driver.set_password_field('password')
-      sleep 1
+      expect(@driver.get_password_value).to be_kind_of(String)
     end
 
     it 'should accept a password confirmation' do
-      expect(@driver.get_password_value).to be_kind_of(String)
-      sleep 1
+      @driver.set_confirmation_password_field('password')
+      expect(@driver.get_confirmation_password_value).to be_kind_of(String)
+    end
+
+    it "submit button should work" do
+      @driver.click_submit
     end
 
   end
